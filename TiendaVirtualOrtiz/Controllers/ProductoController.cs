94f8eb7853d69f4d64991304409c5e148602650f
Lab Simulacion 2 <Lab Simulacion 2@EQUIPO-713>
2026-03-19 17:23:59 -0500

    Agregar archivos de proyecto.

---
+++ TiendaVirtualOrtiz/Controllers/ProductoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TiendaVirtualOrtiz.Data;
+using TiendaVirtualOrtiz.Models;
+
+namespace TiendaVirtualOrtiz.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly TiendaContext _context;
+
+        public ProductoController(TiendaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var productos = _context.Productos
+                .Include(p => p.Categoria)
+                .ToList();
+
+            return View(productos);
+        }
+    }
+}
---
+++ TiendaVirtualOrtiz/Data/TiendaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TiendaVirtualOrtiz.Models;
+
+namespace TiendaVirtualOrtiz.Data
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
