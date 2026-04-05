c226a72912320e1ebac55a6f1803209d514e6a9e
Luna Ortiz <loortiz.3617@unicesmag.edu.co>
2026-04-05 18:25:26 -0500

    Taller en Casa

---
+++ TiendaVirtualOrtiz/Controllers/UsuarioController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TiendaVirtualOrtiz.Data;
+using TiendaVirtualOrtiz.Models;
+
+namespace TiendaVirtualOrtiz.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly TiendaContext _context;
+
+        public UsuarioController(TiendaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var usuarios = _context.Usuarios.ToList();
+
+            return View(usuarios);
+        }
+
+        //Guardar usuario
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //Formulario editar
+        public IActionResult Edit(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+
+            return View(usuario);
+        }
+
+        //Eliminar usuario
+        public IActionResult Delete(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ TiendaVirtualOrtiz/Views/Usuario/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TiendaVirtualOrtiz.Views.Usuario
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
